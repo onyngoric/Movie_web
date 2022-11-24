f0016827aa3e79ba7fd7c4f177b0c6469fa87a63
onyangoric <43799458+onyngoric@users.noreply.github.com>
2022-11-24 11:27:55 +0300

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,56 @@
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10); 
+    value = isNaN(value)? 0 : value;
+    value ++;
+    input.value = value;
+  }
+function decreaseCount(a, b) {
+  var input = b.nextElementSibling;
+  var value = parseInt(input.value, 10); 
+  if (value > 1) {
+    value = isNaN(value)? 0 : value;
+    value --;
+    input.value = value;
+  }
+}
+$(document).ready(function() {
+  "use strict";
+
+  $('ul.nav-tabs > li').click(function(e) {
+    e.preventDefault();
+    $('ul.nav-tabs > li').removeClass('active');
+    $(this).addClass('active');
+  });
+});
+
+//BACK TO TOP BUTTON
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 30 ||
+document.documentElement.scrollTop > 30
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+//loader
+window.addEventListener('load', function() {
+  document.getElementById('loader').style.display = 'none';
+});
